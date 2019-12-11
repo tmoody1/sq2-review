d5d3bbd78a5e6bf7586d61c0ee57a9cdcfa532d7
Tom Clabon <tom.clabon@imanage.com>
2019-12-11 09:50:44 +0000

    Endpoint to add review

--- src/main/java/com/ravn/review/controller/ReviewController.java
+++ src/main/java/com/ravn/review/controller/ReviewController.java
@@ -4,8 +4,9 @@ import com.ravn.review.Review;
 import com.ravn.review.storage.ReviewsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -18,18 +19,17 @@ public class ReviewController {
     @Autowired
     private ReviewsRepository reviewsRepository;
 
-    @RequestMapping("/reviews")
+    @GetMapping("/reviews")
     public List<Review> getBookReviews() {
         return reviewsRepository.findAll();
     }
 
-//    @RequestMapping("/reviews")
-//    @PostMapping
-//    public Review addReview(Review review) {
-//        return reviewRepository.insert(review);
-//    }
+    @PostMapping("/reviews")
+    public Review addReview(Review review) {
+        return reviewsRepository.insert(review);
+    }
 
-    @RequestMapping("/reviews/{isbn}")
+    @GetMapping("/reviews/{isbn}")
     public List<Review> getBookReviews(@PathVariable("isbn") String isbn) {
         return reviewsRepository.findByIsbn(isbn);
     }
